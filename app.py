cb518255ed39534d3f3de8d84d046aac009e542b
shokkun47 <shoki1310@gmail.com>
2024-09-21 17:21:07 +0900

    add new files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
+import subprocess
 
 app = Flask(__name__)
 
@@ -6,5 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/get_item", methods=["POST"])
+def get_item():
+    items = ["apple", "peach", "orange", "pineapples"]
+    item = random.choice(items)
+    return jsonify({"item":item})
+
 if __name__ == "__main__":
+    cmd = f"python whisper_mic/mic.py"
+    subprocess.Popen(cmd.split())
     app.run(debug=True)
